a8c4cc652a40248a21c12a06862d2d07ba63295a
Jared Pace <jared@codewordstudios.com>
2008-10-22 18:07:46 -0400

    Start specing out RMeetup. Added spec_helper to hold a valid api key to run specs against the actual API.

---
+++ spec/client_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+require 'rmeetup'
+
+describe RMeetup::Client, 'trying to access the API before being configured' do
+  it 'should throw an error trying to search' do
+    lambda {
+      RMeetup::Client.fetch(:topics)
+    }.should raise_error(RMeetup::NotConfiguredError)
+  end
+end
+
+describe RMeetup::Client, 'trying to fetch an unknown type' do
+  before do
+    RMeetup::Client.api_key = @api_key
+  end
+  
+  it 'should throw an error' do
+    lambda {
+      RMeetup::Client.fetch(:clowns)
+    }.should raise_error(RMeetup::InvalidRequestTypeError)
+  end
+end
+
